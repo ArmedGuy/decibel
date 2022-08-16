9ce2021be1a1a4b63a1e4daa770a587573151aa4
Johan Jatko <armedguy@ludd.ltu.se>
2022-08-17 01:19:49 +0200

    Change test file to new structure

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from decibel import Decibel
 import common.setup_os
 
-with Decibel(file_delivery_mode="fetch", fetch_base_url="https://prod-decibel.eu-north-1.s3.amazonaws.com") as ds:
+config = Decibel(
+    file_delivery_mode="fetch",
+    fetch_base_url="https://prod-decibel.eu-north-1.s3.amazonaws.com"
+)
+
+with config as ds:
     with ds.hosts():
         common.setup_os.SetupOS(
             datacenter="dh2",
             region="eu-north",
             environment="production"
         )
-    config = ds
